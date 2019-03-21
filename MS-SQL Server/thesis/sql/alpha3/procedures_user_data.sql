20cc8c3b4dd7eb823978294b2681a02aacf59a89
PFlorians <patrikflorians@gmail.com>
2019-03-21 23:31:26 +0100

    Update procedures_user_data.sql

--- MS-SQL Server/thesis/sql/alpha3/procedures_user_data.sql
+++ MS-SQL Server/thesis/sql/alpha3/procedures_user_data.sql
@@ -5,27 +5,10 @@
 -- Data read procedures go here
 use master
 use attendance_dev
-create proc getAttendanceSummaryOfUser
+
+alter proc getMonthlyAttendanceOfUser
 @ulogin varchar(40),
 @monthAtt int = 0,
-@errMsg varchar(255) output
-as
-	set datefirst 1;
-	begin try
-		select asu.hours_worked_month [Worked together], asu.bonus_hours_month[Bonus hours], asu.hours_absent_month[Absences together]
-			from attendance.attusr au
-			join attendance.attendance_record ar on ar.userLogin=au.ulogin
-			join attendance.summary asu on asu.record_id=ar.record_id
-			where au.ulogin=@ulogin and datepart(month, ar.day) = @monthAtt;
-	end try
-	begin catch
-		set @errMsg = ERROR_MESSAGE();
-	end catch;
-go
--- this procedure is a complex view of user's attendance
-create proc getMonthlyAttendanceOfUser
-@ulogin varchar(40),
-@monthAtt int=0, -- cannot initialize by functional expression
 @errMsg varchar(255) output
 as
 	set datefirst 1;
@@ -33,75 +16,128 @@ as
 		if(@monthAtt = 0)
 		begin
 			set @monthAtt = DATEPART(month, convert(date, getdate(), 101));
-
-			select ar.[day] [Day], ar.[from] [From], ar.until [Until], ar.hours_worked_day [Worked]
-			from attendance.attusr au
-			join attendance.attendance_record ar on ar.userLogin=au.ulogin
-			where au.ulogin=@ulogin and datepart(month, ar.[day]) = @monthAtt;
+			select ar.[day] [day], ar.[from] [from], ar.until [until], ar.hours_worked_day [hours_worked_day], ars.shifttype [shifttype]
+				from attendance.attendance_record ar
+				join attendance.recorded_shifts ars on ars.record_id=ar.record_id
+				where DATEPART(month, [day]) = @monthAtt and userLogin = @ulogin
+				order by [day];
 		end;
 		else
 		begin
-			select ar.[day] [Day], ar.[from] [From], ar.until [Until], ar.hours_worked_day [Worked]
-			from attendance.attusr au
-			join attendance.attendance_record ar on ar.userLogin=au.ulogin
-			where au.ulogin=@ulogin and datepart(month, ar.[day]) = @monthAtt;
+			select ar.[day] [day], ar.[from] [from], ar.until [until], ar.hours_worked_day [hours_worked_day], ars.shifttype [shifttype]
+				from attendance.attendance_record ar
+				join attendance.recorded_shifts ars on ars.record_id=ar.record_id
+				where DATEPART(month, [day]) = @monthAtt and userLogin = @ulogin
+				order by [day];
 		end;
 	end try
 	begin catch
 		set @errMsg = ERROR_MESSAGE();
 	end catch;
 go
-create proc getMonthlyBonusOfUser
+begin tran t1
+declare @errMsg varchar(255);
+exec getMonthlyAttendanceOfUser 'pflorian', 2, @errMsg out;
+rollback tran t1
+go
+create proc getMonthlyBonusesOfUser
 @ulogin varchar(40),
-@monthAtt int=0, -- cannot initialize by functional expression
+@monthAtt int = 0,
 @errMsg varchar(255) output
 as
 	set datefirst 1;
 	begin try
 		if(@monthAtt = 0)
 		begin
-			set @monthAtt = datepart(month, convert(date, getdate(), 101));
-			select bon.descr [Bonus], sum(sb.bonus_hours) [Hours together]
-				from attendance.attusr au
-				join attendance.attendance_record ar on ar.userLogin=au.ulogin
-				join attendance.summary asu on asu.record_id=ar.record_id
-				join attendance.summary_bonuses sb on sb.summary_id = asu.summary_id
-				join attendance.bonus as bon on bon.bonus_id=sb.bonus_id
-				where au.ulogin=@ulogin and DATEPART(month, ar.day)=@monthAtt
-				group by bon.descr;
+			set @monthAtt = DATEPART(month, convert(date, getdate(), 101));
+			select asb.[day] [day], asb.bonus_id [id], asb.bonus_hours [bonus_hours], ab.descr [descr]
+			from attendance.attendance_record ar 
+			join attendance.summary_bonuses asb on asb.record_id=ar.record_id
+			join attendance.bonus ab on ab.bonus_id = asb.bonus_id
+			where ar.userLogin = @ulogin and DATEPART(month, ar.[day]) = @monthAtt
+			order by asb.[day] asc;
 		end;
 		else
 		begin
-			select bon.descr [Bonus], sum(sb.bonus_hours) [Hours together]
-				from attendance.attusr au
-				join attendance.attendance_record ar on ar.userLogin=au.ulogin
-				join attendance.summary asu on asu.record_id=ar.record_id
-				join attendance.summary_bonuses sb on sb.summary_id = asu.summary_id
-				join attendance.bonus as bon on bon.bonus_id=sb.bonus_id
-				where au.ulogin=@ulogin and DATEPART(month, ar.day)=@monthAtt
-				group by bon.descr;
+			select asb.[day] [day], asb.bonus_id [id], asb.bonus_hours [bonus_hours], ab.descr [descr]
+			from attendance.attendance_record ar 
+			join attendance.summary_bonuses asb on asb.record_id=ar.record_id
+			join attendance.bonus ab on ab.bonus_id = asb.bonus_id
+			where ar.userLogin = @ulogin and DATEPART(month, ar.[day]) = @monthAtt
+			order by asb.[day] asc;
 		end;
 	end try
 	begin catch
 		set @errMsg = ERROR_MESSAGE();
 	end catch;
 go
-create proc getUsersPublicHolidaysByMonth
+--get and return monthly absences
+create proc getMonthlyAbsencesOfUser
 @ulogin varchar(40),
-@monthAtt int =0,
+@monthAtt int = 0,
 @errMsg varchar(255) output
 as
-	if(@monthAtt = 0)
-	begin
-		set @monthAtt = datepart(month, convert(date, getdate(), 101));
-		select ph.[date] as [date], sph. from attendance.attusr atu
-			join attendance.attendance_record ar on ar.userLogin=atu.ulogin
-			join attendance.summary asu on asu.record_id=ar.record_id
-			join attendance.summary_public_holidays sph on sph.summary_id=asu.summary_id
-			join attendance.public_holidays ph on ph.id=sph.public_holiday_id
-			where atu.ulogin = @ulogin
-			group by
-	end;
-	else
-	begin
-	end;
+	set datefirst 1;
+	begin try
+		if(@monthAtt = 0)
+		begin
+			set @monthAtt = DATEPART(month, convert(date, getdate(), 101));
+			select asa.day_of_absence [day_of_absence], asa.absence_type [absence_type], asa.hours_absent [hours_absent],
+			aa.descr [descr]
+			from attendance.attendance_record ar 
+			join attendance.summary_absence asa on asa.record_id=ar.record_id
+			join attendance.absence aa on aa.[type] = asa.[absence_type]
+			where ar.userLogin = @ulogin and DATEPART(month, ar.[day]) = @monthAtt;
+		end;
+		else
+		begin
+			select asa.day_of_absence [day_of_absence], asa.absence_type [absence_type], asa.hours_absent [hours_absent],
+			aa.descr [descr]
+			from attendance.attendance_record ar 
+			join attendance.summary_absence asa on asa.record_id=ar.record_id
+			join attendance.absence aa on aa.[type] = asa.[absence_type]
+			where ar.userLogin = @ulogin and DATEPART(month, ar.[day]) = @monthAtt;
+		end;
+	end try
+	begin catch
+		set @errMsg = ERROR_MESSAGE();
+	end catch;
+go
+begin tran t2
+	declare @errMsg varchar(255);
+	exec getMonthlyBonusesOfUser 'pflorian', 5, @errMsg out;
+	exec getMonthlyAbsencesOfUser 'pflorian', 2, @errMsg out;
+rollback tran t2
+go
+alter proc getMonthlySummaryOfUser
+@ulogin varchar(40),
+@monthAtt int = 0,
+@errMsg varchar(255) output
+as
+	declare @summaryId int;
+	set @summaryId=(select top 1 asu.summary_id from attendance.attusr as au
+			join attendance.attendance_record as ar on ar.userLogin = au.ulogin
+			join attendance.summary as asu on asu.record_id=ar.record_id
+			where ar.userLogin = @ulogin and (@monthAtt = MONTH(ar.[day]))
+			order by ar.record_id asc);
+	begin try
+		if(@summaryId is not null)
+		begin
+			select hours_worked_month [worked], bonus_hours_month [bonus], hours_absent_month [absent]
+			from attendance.summary ass
+			where summary_id=@summaryId;
+		end;
+		else
+		begin
+			set @errMsg = 'Summary for this month was not found';
+			throw 57001, @errMsg, 1;
+		end;
+	end try
+	begin catch
+		set @errMsg=ERROR_MESSAGE();
+	end catch;
+go
+begin tran t3
+	declare @errMsg varchar(255);
+	exec getMonthlySummaryOfUser 'pflorian', 2, @errMsg out;
+rollback tran t3
